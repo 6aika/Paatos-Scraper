3b0aa6362234855d816bfa6fa3b172aa76863283
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-04-04 09:17:13 +0300

    Implemented pdf parsing

--- scrapers/abstract-pdf-scraper.js
+++ scrapers/abstract-pdf-scraper.js
@@ -17,14 +17,35 @@
       this.pdfParser.on('pdfParser_dataError', this.onPdfParseError.bind(this));
       this.pdfParser.on('pdfParser_dataReady', this.onPdfParseReady.bind(this));
       this.pdfParser.loadPDF(this.pdfFile);
+      this._pdfData = null;
+      this._pdfDataPromise = null;
     }
     
     onPdfParseError(errData) {
-      
+      if (this._pdfDataPromise !== null) {
+        this._pdfDataPromise.reject(errData);
+        this._pdfDataPromise = null;
+      }
     }
     
     onPdfParseReady(pdfData) {
-      
+      if (this._pdfDataPromise !== null) {
+        this._pdfDataPromise.resolve(pdfData);
+        this._pdfData = pdfData;
+        this._pdfDataPromise = null;
+      } else {
+        this._pdfData = pdfData;
+      }
+    }
+    
+    get pdfData() {
+      return new Promise((resolve, reject) => {
+        if (this._pdfData === null) {
+          this._pdfDataPromise = new Promise((resolve, reject));
+        } else {
+          resolve(this._pdfData);
+        }
+      });
     }
     
   }
