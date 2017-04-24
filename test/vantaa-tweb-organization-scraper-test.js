23f267651eac66e76aebc87c662d82d4d3f52fc8
Antti Leppä <antti.leppa@metatavu.fi>
2017-04-24 09:28:25 +0300

    Added tests for extracting vantaa organizations

---
+++ test/vantaa-tweb-organization-scraper-test.js
@@ -0,0 +1,35 @@
+/*jshint esversion: 6 */
+/* global __dirname */
+
+(function() {
+  'use strict';
+  
+  const chai = require('chai');
+  const nock = require('nock');
+  const expect = chai.expect;
+  const assert = chai.assert;
+  
+  chai.use(require('chai-as-promised'));
+  
+  const VantaaTwebHtmlScraper = require(__dirname + '/../scrapers/tweb/vantaa/vantaa-tweb-html-scraper');
+
+  const vantaaToimielimet = require(__dirname + '/data/vantaa_toimielimet');
+
+  describe('Vantaa Tweb Html Organization Scraper tests', () => {
+    
+    var htmlTestScraper = new VantaaTwebHtmlScraper({
+      "host": "localhost"
+    })
+    
+    it('Test organizations scraping', () => {
+      nock('http://localhost')
+        .get('/ktwebbin/dbisa.dll/ktwebscr/epj_tek_tweb.htm')
+        .replyWithFile(200, __dirname + '/data/vantaa_tweb_haku.html');
+      
+      return expect(Promise.resolve(htmlTestScraper.extractOrganizations()))
+        .to.eventually.eql(vantaaToimielimet);
+    });
+    
+  });
+  
+})();
