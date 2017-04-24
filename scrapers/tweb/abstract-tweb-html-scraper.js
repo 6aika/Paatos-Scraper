c6f80dbedf3e4d0da12dd7a4cf77a26f3c5e4057
Antti Leppä <antti.leppa@metatavu.fi>
2017-04-24 08:37:18 +0300

    Abstracted organizations scraping from Oulu to Tweb

--- scrapers/tweb/abstract-tweb-html-scraper.js
+++ scrapers/tweb/abstract-tweb-html-scraper.js
@@ -4,6 +4,8 @@
 (function() {
   'use strict';
 
+  const _ = require('lodash');
+  const util = require('util');
   const AbstractHtmlScraper = require(__dirname + '/../abstract-html-scraper');
   const request = require('request');
   
@@ -14,6 +16,44 @@
     
     constructor() {
       super();
+    }
+    
+    /**
+     * Returns a promise for scraped organizations.
+     * 
+     * Returned data is ordered in same order that it is in html page. 
+     */
+    extractOrganizations() {
+      return new Promise((resolve, reject) => {
+        var options = {
+          url: util.format("http://%s%s", this.options.host, this.options.searchFormPath),
+          encoding: this.options.encoding,
+          requestInterval: this.options.requestInterval
+        };
+        
+        this.getParsedHtml(options)
+          .then(($) => {
+            var options = $('form[name="form1"] select[name="kirjaamo"] option').filter((index, option) => {
+              return $(option).val();
+            });
+            
+            var organizations = _.map(options, (option) => {
+              var id = $(option).val(); 
+              var name = $(option).text();
+              return {
+                "sourceId": id,
+                "classification": this.guessClassification(name),
+                "name": name,
+                "founding_date": null,
+                "dissolution_date": null,
+                "parent": null
+              };
+            });
+            
+            resolve(organizations);
+          })
+          .catch(reject);         
+      });
     }
     
   }
--- scrapers/tweb/oulu/oulu-tweb-html-scraper.js
+++ scrapers/tweb/oulu/oulu-tweb-html-scraper.js
@@ -35,44 +35,6 @@
     }
     
     /**
-     * Returns a promise for organizations html page.
-     * 
-     * Returned data is ordered in same order that it is in html page. 
-     */
-    extractOrganizations() {
-      return new Promise((resolve, reject) => {
-        var options = {
-          url: util.format("http://%s%s", this.options.host, this.options.searchFormPath),
-          encoding: this.options.encoding,
-          requestInterval: this.options.requestInterval
-        };
-        
-        this.getParsedHtml(options)
-          .then(($) => {
-            var options = $('form[name="form1"] select[name="kirjaamo"] option').filter((index, option) => {
-              return $(option).val();
-            });
-            
-            var organizations = _.map(options, (option) => {
-              var id = $(option).val(); 
-              var name = $(option).text();
-              return {
-                "sourceId": id,
-                "classification": this.guessClassification(name),
-                "name": name,
-                "founding_date": null,
-                "dissolution_date": null,
-                "parent": null
-              };
-            });
-            
-            resolve(organizations);
-          })
-          .catch(reject);         
-      });
-    }
-    
-    /**
      * Returns a promise for organization events.
      * 
      * Returned data is ordered in same order that it is in html page. 
