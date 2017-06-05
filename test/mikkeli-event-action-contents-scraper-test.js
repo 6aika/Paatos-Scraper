15462caef932cd0f6251b6dae6bcc81398d58723
Antti Leppä <antti.leppa@metatavu.fi>
2017-06-05 20:59:33 +0300

    Added tests for scraping action contents from Mikkeli

---
+++ test/mikkeli-event-action-contents-scraper-test.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 6 */
+/* global __dirname */
+
+(function() {
+  'use strict';
+  
+  const util = require('util');
+  const chai = require('chai');
+  const nock = require('nock');
+  const Promise = require('bluebird');
+  const moment = require('moment');
+  const expect = chai.expect;
+  const assert = chai.assert;
+  
+  chai.use(require('chai-as-promised'));
+  
+  const MikkeliCasemScraper = require(__dirname + '/../scrapers/casem/mikkeli/mikkeli-casem-scraper');  
+  const mikkeliKh170605Contents229 = require(__dirname + '/data/mikkeli/mikkeli_kh_170605_contents_229');
+  
+  describe('Mikkeli events contents scraper tests', () => {
+    
+    it('Test event action contents scraping', () => {
+      const testScraper = new MikkeliCasemScraper({
+        "host": "localhost"
+      });
+    
+      return expect(Promise.resolve(testScraper.extractOrganizationEventActionContents("11217", "12381", "4027")))
+        .to.eventually.eql(mikkeliKh170605Contents229);
+    });
+    
+  });
+  
+})();
